ad331690083ed903f37429c3c575fc726436acec
Xavier <tusaromix@gmail.com>
2021-02-03 19:49:49 +0100

    03/02/2021 Taller

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,21 @@
 //
-const { series, src, dest, watch } = require('gulp');
+const { series, src, dest, watch, parallel } = require('gulp');
 const sass = require('gulp-sass');
+const imagemin = require('gulp-imagemin');
+const notify = require('gulp-notify');
+const webp = require('gulp-webp');
+const concat = require('gulp-concat');
+
+const paths = {
+    imagenes: 'src/img/**/*',
+    scss: 'src/scss/**/*.scss',
+    js: 'src/js/**/*.js'
+}
 
 // Funcion que compila SASS
 
 function css() {
-    return src('src/scss/app.scss')
+    return src(paths.scss)
         .pipe(sass())
         .pipe(dest('./build/css'))
 
@@ -13,7 +23,7 @@ function css() {
 }
 
 function minificarcss() {
-    return src('src/scss/app.scss')
+    return src(paths.scss)
         .pipe(sass({
 
             outputStyle: 'compressed'
@@ -23,11 +33,35 @@ function minificarcss() {
 
 }
 
-function watchArchivos() {
-    watch('src/scss/**/*.scss', css); //* es igual a la carpeta actual - ** = TODOS los archivos con esa extensión
+function javascript() {
+    return src(paths.js)
+        .pipe(concat('bundle.js'))
+        .pipe(dest('./build/js'));
 
+}
+
+function imagenes() {
+    return src(paths.imagenes) //Entra a img y busca todas las imagenes **/* 
+        .pipe(imagemin())
+        .pipe(dest('./build/img'))
+        .pipe(notify({ message: 'Imagen Minificada' }));
+}
+
+function versionWebp() {
+    return src(paths.imagenes)
+        .pipe(webp())
+        .pipe(dest('./build/img'))
+        .pipe(notify({ message: 'Versión webP lista' }));
+}
+
+function watchArchivos() {
+    watch(paths.scss, css); //* es igual a la carpeta actual - ** = TODOS los archivos con esa extensión
+    watch(paths.js, javascript);
 }
 
 exports.css = css;
 exports.minificarcss = minificarcss;
+exports.imagenes = imagenes;
 exports.watchArchivos = watchArchivos;
+
+exports.default = series(css, javascript, imagenes, versionWebp, watchArchivos);
